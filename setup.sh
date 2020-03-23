8d8e7862abeb8948e39d8134e78fd6ed177927ac
cuichang <cuichang@gmail.com>
2020-03-23 18:34:30 +0800

    update vim config shell

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 rm ./installer.sh
+
+curl https://raw.githubusercontent.com/changcui/vim-config/master/init.vim > ~/.vimrc
+
